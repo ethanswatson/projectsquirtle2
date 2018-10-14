8eb0adce31c0030b8f333c1675e1ad8b49bc47a9
Brady Svedin <brady.svedin@aggiemail.usu.edu>
2018-10-14 14:58:30 -0600

    Set up a websocket connection on profile, nothing special it only opens one up, to show how it works and ready for future use.

--- src/quizapp/consumers.py
+++ src/quizapp/consumers.py
@@ -1,1 +1,17 @@
 #This file is very similar to urls. It handels all the websockets messages that are passed to each consumer class. like urls are passed to views, websockets are passed to consumers.
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+from django.contrib.auth.models import User
+from .models import Quiz
+
+class AccountConsumer(AsyncWebsocketConsumer):
+    
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+        
+    async def receive(self, text_data):
+        pass
