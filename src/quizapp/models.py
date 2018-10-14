25d51d1bb5a606eb99c8054e90923054b36524bb
Ethan Watson <ethanswatson@aggiemail.usu.edu>
2018-10-14 10:31:35 -0600

    Added properties and getters/setters to Quiz

--- src/quizapp/models.py
+++ src/quizapp/models.py
@@ -2,4 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Quiz(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    _owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    _quizName = models.CharField(max_length=20)
+    _quizDescription = models.TextField()
+
+    def getOwner(self):
+        return self._owner
+    def getQuizName(self):
+        return self._quizName
+    def getQuizDescription(self):
+        return self._quizDescription
+
+    def setOwner(self, newOwner):
+        self._owner = newOwner
+    def setQuizName(self, newName):
+        self._quizName = newName
+    def setQuizDescription(self, newDescription):
+        self._quizDescription = newDescription
+
