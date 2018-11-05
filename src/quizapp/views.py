00d50d77e64c787d363768ccccc77ea34514bce4
Cory Kaye <coryk.kaye@gmail.com>
2018-11-04 18:47:10 -0700

    Start Quiz opens new tab to /session/room_name

--- src/quizapp/views.py
+++ src/quizapp/views.py
@@ -17,6 +17,17 @@ def joinQuiz(request):
     form = JoinQuizForm()
     return render(request, 'quizapp/joinquiz.html', {'form': form})
 
+#WebSocket for teacher/student views
+
+def roomMain(request, room_name):
+    return render(request, 'quizapp/sessionmain.html', {
+            'room_name_json': mark_safe(json.dumps(room_name))
+        })
+
+def roomJoin(request, room_name):
+    return render(request, 'quizapp/sessionjoin.html', {
+            'room_name_json': mark_safe(json.dumps(room_name))
+        })
 
 #Login View is handled by django, as well as logout, and password vies. you do need to create the html templates for each, but it handles the rest. Django authentication is described here https://docs.djangoproject.com/en/2.1/topics/auth/default/
 
