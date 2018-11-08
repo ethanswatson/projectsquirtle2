f71b5e3ece87b6074b644ef69b729162966520d7
Ethan Watson <ethanswatson@aggiemail.usu.edu>
2018-11-07 17:35:10 -0700

    Rewrote teacher question page with JS DOM manipulation

--- src/quizapp/static/quizapp/scripts/teacherquestion.js
+++ src/quizapp/static/quizapp/scripts/teacherquestion.js
@@ -1,17 +1,46 @@
-var answers = document.querySelectorAll('.answer-box');
-
-var labels = ['A','B','C','D','E','F'];
-
-var i;
-for (i = 0; i < labels.length; i++) {
-    var answer = answers[i];
-    var label = labels[i];
-    answer.setAttribute('value', label);
-    answerText = answer.querySelector('.answer-option');
-    answerText.innerHTML = label + ': ' + answerText.innerHTML;
-    answer.onclick = submitAnswer;
+// Dummy data. Will be replaced with data from websockets
+let question = {
+    questionText: 'A Fake Question',
+    answers: [
+        {
+            text:'Option 1'
+        },
+        {
+            text: 'Option 2'
+        },
+        {
+            text: 'Option 3'
+        },
+        {
+            text: 'Option 4'
+        },
+        {
+            text: 'Option 5'
+        },
+        {
+            text: 'Option 6'
+        },
+    ]
 }
 
-function submitAnswer() {
-
+let main = document.querySelector('main');
+let questionTextSection = document.createElement('section');
+let questionText = document.createElement('p');
+questionText.setAttribute('class', 'question-text');
+questionText.textContent = question.questionText;
+main.appendChild(questionTextSection);
+let answerSection = document.createElement('section');
+answerSection.setAttribute('class', 'answer-section');
+let labels = ['A','B','C','D','E','F'];
+for (let i = 0; i < question.answers.length && i < labels.length; i++) {
+    let answer = question.answers[i];
+    let label = labels[i];
+    let answerBox = document.createElement('div');
+    answerBox.setAttribute('class', 'answer-box');
+    answerBox.setAttribute('value', label);
+    let answerText = document.createElement('p');
+    answerText.textContent = label + ': ' + answer.text;
+    answerBox.appendChild(answerText);
+    answerSection.appendChild(answerBox);
 }
+main.appendChild(answerSection);
