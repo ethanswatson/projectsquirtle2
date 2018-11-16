6b06c7402575d70cf561267b1c6392066e974748
Brady Svedin <brady.svedin@aggiemail.usu.edu>
2018-11-16 12:12:28 -0700

    made sure tests work with the merge into master. Username is sent, and votes are sent, and the 'question' or input from test screen is sent.

--- src/quizapp/static/quizapp/scripts/teachersession.js
+++ src/quizapp/static/quizapp/scripts/teachersession.js
@@ -7,60 +7,60 @@ var chatSocket;
 var connectToSocket = function (roomName) {
     chatSocket = new WebSocket(
         'ws://' + window.location.host +
-        '/ws/quizapp/' + roomName + '/');
+        '/ws/quizapp/host/' + roomName + '/');
 
-        chatSocket.onmessage = function(e) {
-            var data = JSON.parse(e.data);
-                   
-            if (data['msg_type']=='0') {
-                var message = data['message'];
-                document.querySelector('#chat-log').value += ('sent: ' + message + '\n');
+    chatSocket.onmessage = function(e) {
+        var data = JSON.parse(e.data);
+        var message = data['message'];
+        if (data['msgType']=='msgJoin') {
+            
+            document.querySelector('#log').value += message['userName'] + " has join the quiz\n";
+        }
+        if (data['msgType']=='msgVote') {
+            answerID = message['answerID']
+            if (answerID == '0') {
+                voteA +=1;
+                document.getElementById("dispVoteA").innerHTML = "Votes for A: " + voteA;
             }
-            if (data['msg_type']=='1') {
-                var message = data['message'];
-                if (message == 'A') {
-                    voteA +=1;
-                    document.getElementById("dispVoteA").innerHTML = "Votes for A: " + voteA;
-                }
-                if (message == 'B') {
-                    voteB +=1;
-                    document.getElementById("dispVoteB").innerHTML = "Votes for B: " + voteB;
-                }
-                if (message == 'C') {
-                    voteC +=1;
-                    document.getElementById("dispVoteC").innerHTML = "Votes for C: " + voteC;
-                }
-                if (message == 'D') {
-                    voteD +=1;
-                    document.getElementById("dispVoteD").innerHTML = "Votes for D: " + voteD;
-                }
+            if (answerID == '1') {
+                voteB +=1;
+                document.getElementById("dispVoteB").innerHTML = "Votes for B: " + voteB;
             }
+            if (answerID == '2') {
+                voteC +=1;
+                document.getElementById("dispVoteC").innerHTML = "Votes for C: " + voteC;
+            }
+            if (answerID == '3') {
+                voteD +=1;
+                document.getElementById("dispVoteD").innerHTML = "Votes for D: " + voteD;
+            }
+        }
+    };
         
-            if (data['msg_type']=='2') {
-                var altMessage = data['message'];
-                document.querySelector('#chat-log').value += (altMessage + '\n');
-            };
-        };
-        
-        chatSocket.onclose = function(e) {
-                console.error('Chat socket closed unexpectedly');
-        };
+    chatSocket.onclose = function(e) {
+            console.error('Chat socket closed unexpectedly');
+    };
+
+
+    document.querySelector('#questionInput').focus();
+    document.querySelector('#questionInput').onkeyup = function(e) {
+    	if (e.keyCode === 13) {  // enter, return
+		    document.querySelector('#questionInputSubmit').click();
+	    }
+    };
+
+
+
+    document.querySelector('#questionInputSubmit').onclick = function(e) {
+   	    var messageInputDom = document.querySelector('#questionInputSubmit');
+        var message = messageInputDom.value;
+        console.log("pressed send");
+	    chatSocket.send(JSON.stringify({
+    		'message': message,
+		    'msgType': 'msgQuestion'
+	    }));
+
+	    messageInputDom.value = '';
 };
 
-document.querySelector('#chat-message-input').focus();
-document.querySelector('#chat-message-input').onkeyup = function(e) {
-	if (e.keyCode === 13) {  // enter, return
-		document.querySelector('#chat-message-submit').click();
-	}
-};
-
-document.querySelector('#chat-message-submit').onclick = function(e) {
-   	var messageInputDom = document.querySelector('#chat-message-input');
-	var message = messageInputDom.value;
-	chatSocket.send(JSON.stringify({
-		'message': message,
-			'msg_type': '0'
-	}));
-
-	messageInputDom.value = '';
 };
