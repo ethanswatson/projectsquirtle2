57e347b7b2bb30fc18985bbea38f6fd74933b34d
Ethan Watson <ethanswatson@aggiemail.usu.edu>
2019-06-22 15:48:54 -0700

    added whitenoise to serve static files

--- src/backend/projectsquirtle/settings.py
+++ src/backend/projectsquirtle/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'projectsquirtle.urls'
@@ -132,3 +133,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
